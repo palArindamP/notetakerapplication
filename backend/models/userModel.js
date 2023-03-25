116bb71e858293463df6ce17ccc3f41a31e66932
palArindamP <arindampal955@gmail.committed>
2023-03-25 12:25:33 +0530

    added

---
+++ backend/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = reqiure('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        require: true,
+        trim: true
+    }
+
+})
+
+module.exports = mongoose.model('Users', userSchema);
